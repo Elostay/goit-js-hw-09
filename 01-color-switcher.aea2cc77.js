1587ff5ca4a9ea434de5b41ee01827a1b626fe36
Elostay <97159101+Elostay@users.noreply.github.com>
2023-09-22 18:47:23 +0000

    Deploying to gh-pages from @ Elostay/goit-js-hw-09@c9dc122b5aa93a57cf679ade6a2ea5e2de3b9201 🚀

---
+++ 01-color-switcher.aea2cc77.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),d=document.querySelector("body");t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,n=setInterval(a,1e3)})),e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,clearInterval(n)}));let n=null;function a(){d.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}e.disabled=!0;
+//# sourceMappingURL=01-color-switcher.aea2cc77.js.map
